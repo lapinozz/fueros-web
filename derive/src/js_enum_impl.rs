155b34ffe23b93198117071533ccf060de881d09
jazzfool <saveuselon@gmail.com>
2021-12-15 18:09:17 +1100

    better enum bindings

--- derive/src/js_enum_impl.rs
+++ derive/src/js_enum_impl.rs
@@ -4,82 +4,74 @@ use {proc_macro::TokenStream, quote::quote};
 
 pub fn js_enum_impl(input: TokenStream) -> TokenStream {
     let ast: syn::ItemImpl = syn::parse(input).unwrap();
+    let self_ty = &*ast.self_ty;
 
-    let ident = if let syn::Type::Path(path) = &*ast.self_ty {
-        path.path.get_ident().unwrap()
-    } else {
-        panic!("this ain't no enum impl")
-    };
-
-    let out_ident = quote::format_ident!("Js{}", ident);
-    let out_methods = ast
+    let methods = ast
         .items
         .iter()
-        .filter_map(|x| {
-            if let syn::ImplItem::Method(method) = x {
-                let vis = &method.vis;
-                let sig = &method.sig;
-                let fn_name = &sig.ident;
-
-                let is_mut = method
+        .filter_map(|item| {
+            if let syn::ImplItem::Method(method) = item {
+                if let Some(syn::FnArg::Receiver(receiver)) = method
                     .sig
                     .inputs
                     .iter()
-                    .find_map(|arg| {
-                        if let syn::FnArg::Receiver(arg) = arg {
-                            Some(arg)
-                        } else {
-                            None
-                        }
-                    })?
-                    .mutability
-                    .is_some();
-
-                let non_self_args = sig
-                    .inputs
-                    .iter()
-                    .filter_map(|arg| {
-                        if let syn::FnArg::Typed(arg) = arg {
-                            Some(arg.pat.as_ref())
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-
-                Some(if is_mut {
-                    quote! {
-                        #vis #sig {
-                            let mut this: #ident = std::mem::take(self).into();
-                            let out = this.#fn_name(#(#non_self_args,)*);
-                            *self = this.into();
-                            out
-                        }
-                    }
+                    .find(|input| matches!(input, syn::FnArg::Receiver(_)))
+                {
+                    Some((method, receiver.mutability.is_some()))
                 } else {
-                    quote! {
-                        #vis #sig {
-                            let this: #ident = self.clone().into();
-                            this.#fn_name(#(#non_self_args,)*)
-                        }
-                    }
-                })
+                    None
+                }
             } else {
                 None
             }
         })
-        .collect::<Vec<_>>();
-
-    {
-        quote! {
-            #ast
-
-            #[allow(non_snake_case)]
-            #[wasm_bindgen]
-            impl #out_ident {
-                #(#out_methods)*
+        .map(|(method, is_mut)| {
+            let params = method
+                .sig
+                .inputs
+                .iter()
+                .filter_map(|arg| {
+                    if let syn::FnArg::Typed(arg) = arg {
+                        Some(arg)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+            let original_method = &method.sig.ident;
+            let method_ident = heck::AsLowerCamelCase(method.sig.ident.to_string()).to_string();
+            let method_ident = syn::Ident::new(&method_ident, proc_macro2::Span::call_site());
+            let return_type = &method.sig.output;
+            let fwd_args = params.iter().map(|param| &*param.pat).collect::<Vec<_>>();
+            if is_mut {
+                quote! {
+                    #[allow(non_snake_case)]
+                    pub fn #method_ident(obj: ::js_sys::Object, #(#params,)*) #return_type {
+                        let mut this = (&*obj).into_serde::<Self>().unwrap();
+                        let out = this.#original_method(#(#fwd_args,)*);
+                        ::js_sys::Object::assign(
+                            &obj,
+                            &::js_sys::Object::from(::wasm_bindgen::JsValue::from_serde(&this).unwrap()),
+                        );
+                        out
+                    }
+                }
+            } else {
+                quote! {
+                    #[allow(non_snake_case)]
+                    pub fn #method_ident(obj: ::js_sys::Object, #(#params,)*) #return_type {
+                        (&*obj).into_serde::<Self>().unwrap().#original_method(#(#fwd_args,)*)
+                    }
+                }
             }
+        }).collect::<Vec<_>>();
+
+    TokenStream::from(quote! {
+        #ast
+
+        #[wasm_bindgen]
+        impl #self_ty {
+            #(#methods)*
         }
-    }
-    .into()
+    })
 }
--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,12 +1,6 @@
-mod js_enum;
 mod js_enum_impl;
 
 use proc_macro::TokenStream;
-
-#[proc_macro_derive(JsEnum)]
-pub fn js_enum(input: TokenStream) -> TokenStream {
-    js_enum::js_enum(input)
-}
 
 #[proc_macro_attribute]
 pub fn js_enum_impl(_attr: TokenStream, input: TokenStream) -> TokenStream {
