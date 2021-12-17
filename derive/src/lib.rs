f4b48309714bfd84fe1ed78f66920f71a62d8d73
jazzfool <saveuselon@gmail.com>
2021-12-18 03:34:46 +1100

    proto

--- derive/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod js_enum_impl;
-
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn js_enum_impl(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    js_enum_impl::js_enum_impl(input)
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,48 @@
-use fueros_derive::js_enum_impl;
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
-// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen]
+pub struct EdgeUpdate {
+    pub x: i32,
+    pub y: i32,
+    pub set: bool,
+}
+
+#[wasm_bindgen]
+pub struct Callbacks {
+    set_edges: js_sys::Function,
+}
+
+#[wasm_bindgen]
+impl Callbacks {
+    #[wasm_bindgen(setter)]
+    pub fn set_edges(&mut self, f: js_sys::Function) {
+        self.set_edges = f;
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_game(callbacks: Callbacks) {
+    let game = Game {
+        set_edges: Box::new(move |edges: &[EdgeUpdate]| {
+            use wasm_bindgen::convert::{FromWasmAbi, IntoWasmAbi};
+            callbacks.set_edges.call1(
+                &JsValue::NULL,
+                &edges
+                    .into_iter()
+                    .map(JsValue::from)
+                    .collect::<js_sys::Array>(),
+            );
+        }),
+    };
+}
+
+pub struct Game {
+    set_edges: Box<dyn Fn(&[EdgeUpdate])>,
+}
+
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello my world!"));
-
     Ok(())
-}
-
-#[derive(Deserialize, Serialize)]
-pub enum Edge {
-    #[serde(rename_all = "camelCase")]
-    Set { player_idx: u32 },
-    #[serde(rename_all = "camelCase")]
-    Unset,
-}
-
-#[js_enum_impl]
-impl Edge {
-    pub fn is_set(&self) -> bool {
-        matches!(self, Edge::Set { .. })
-    }
-
-    pub fn change_player(&mut self, new_player_idx: u32) -> u32 {
-        if let Edge::Set { player_idx } = self {
-            let prev = *player_idx;
-            *player_idx = new_player_idx;
-            prev
-        } else {
-            0
-        }
-    }
-}
-
-#[wasm_bindgen()]
-pub fn test() -> JsValue {
-    // Your code goes here!
-    JsValue::from_str("Hello my world!")
 }
