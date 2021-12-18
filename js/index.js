190eda13c5472ccfc751ca9c8c905b63d4bbd48b
lapinozz <monstrefou@gmail.com>
2021-12-18 12:05:31 -0500

    Bnechmark

--- js/index.js
+++ js/index.js
@@ -13,45 +13,70 @@ class Test
     }
 }
 
+function Test2(obj)
+{
+    let t = {}
+    Object.assign(t, obj)
+    return t;
+}
+
 async function main() {
 
     menu();
 
-    let cbs = new Callbacks();
+    const benchmarkCount = 100;
 
-    let edgeUpdate;
-    let es;
-
-    cbs.set_edges((edges) => {
-        const memory = new DataView(shared_memory().buffer);
-
-        const edgesPtr = memory.getUint32(edges.ptr + 4 + 0, true);
-        const edgesLen = memory.getUint32(edges.ptr + 4 + 4, true);
-        edgeUpdate = new EdgeUpdate(edgesPtr, memory); 
-
-        es = new EdgeUpdateArray(edges.ptr, memory); 
-    });
-
-    let t1 = performance.now();
-    run_game(cbs);
-    let t2 = performance.now();
-    console.log(`${t2 - t1} ms`);
-    console.log(edgeUpdate)
-    console.log(es)
-
-    const board = new Board(10, 10);
-    document.body.appendChild(board.app.view);
-
-
-    let tl1 = performance.now();
-    let a = [];
-    for(let i = 0; i < 100000; i++)
+    let rustTime = 0;
+    for(let i = 0; i < benchmarkCount; i++)
     {
-        a.push(new Test({x: i, y: i % 10, set: !!(i % 2)}));
+        let cbs = new Callbacks();
+
+        let edgeUpdate;
+        let es;
+
+        cbs.set_edges((edges) => {
+            const memory = new DataView(shared_memory().buffer);
+
+            const edgesPtr = memory.getUint32(edges.ptr + 4 + 0, true);
+            const edgesLen = memory.getUint32(edges.ptr + 4 + 4, true);
+            edgeUpdate = new EdgeUpdate(edgesPtr, memory); 
+
+            es = new EdgeUpdateArray(edges.ptr, memory); 
+        });
+
+        let t1 = performance.now();
+        run_game(cbs);
+        let t2 = performance.now();
+        const time = t2 - t1;
+        rustTime += time / benchmarkCount;
+        //console.log(`${t2 - t1} ms`);
+        //console.log(edgeUpdate)
+        //console.log(es)
+
     }
-    let tl2 = performance.now();
-    console.log(`${tl2 - tl1} ms`);
-    console.log(a)
+
+    //const board = new Board(10, 10);
+    //document.body.appendChild(board.app.view);
+
+
+    let jsTime = 0;
+    for(let i = 0; i < benchmarkCount; i++)
+    {
+        let tl1 = performance.now();
+        let a = [];
+        for(let i = 0; i < 100000; i++)
+        {
+            a.push(new Test({x: i, y: i % 10, set: !!(i % 2)}));
+        }
+        let tl2 = performance.now();
+        const time = tl2 - tl1;
+        jsTime += time / benchmarkCount;
+        //console.log(`${tl2 - tl1} ms`);
+        //console.log(a)
+    }
+
+    console.log({rustTime, jsTime});
+    console.log(rustTime /  jsTime);
 
 }
 
